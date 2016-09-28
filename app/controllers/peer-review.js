859bb86dd53c7f1c10910ab5f6ebf1f78a53fb24
jnayak1 <jdn4ae@virgnia.edu>
2016-09-28 01:51:52 -0400

    Login and routes

--- app/controllers/peer-review.js
+++ app/controllers/peer-review.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions:{
+		fileDetail(file) {
+	        this.transitionToRoute('file',
+	                               this.get('node'),
+	                               file.get('provider'),
+	                               file);
+	    },
+
+	    nodeDetail(node) {
+	        this.transitionToRoute('nodes.detail', node);
+	    }
+	}
+	
 });
