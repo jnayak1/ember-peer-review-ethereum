243b6b34e245625146a6f77d6993c8ecfb327a2a
Jesse Nayak <jdn4ae@virginia.edu>
2016-10-26 10:59:28 -0400

    Keep track of contracts instead of addresses

--- public/contracts/PeerReviewDAO.sol
+++ public/contracts/PeerReviewDAO.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.4.2;
 import "PeerReviewContract";
 
 contract PeerReviewDAO {
-    address[] peerReviewContracts;
+    PeerReview[] public peerReviewContracts;
+
+    event peerReviewCreated();
 
     function createPeerReviewContract(address initJournal, bytes initFileHash) payable {
         // https://solidity.readthedocs.io/en/develop/control-structures.html#creating-contracts-via-new
-        // Cannot limit the amount of gas
+        // Cannot limit the amount of gas!
         
         PeerReview newPR = (new PeerReview).value(msg.value)(initJournal, initFileHash);
-        peerReviewContracts.push(newPR.address);
+        peerReviewContracts.push(address(newPR));
+
+        peerReviewCreated();
     }
 }
