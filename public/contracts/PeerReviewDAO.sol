ecf77bcb28c202a7509c9d7cbece0f308f3ee5df
Jesse Nayak <jdn4ae@virginia.edu>
2016-10-26 09:57:16 -0400

    Create PeerReview contract from DAO

--- public/contracts/PeerReviewDAO.sol
+++ public/contracts/PeerReviewDAO.sol
@@ -1,3 +1,14 @@
-contract PeerReviewDAO {
+pragma solidity ^0.4.2;
 
+import "PeerReviewContract";
+
+contract PeerReviewDAO {
+    address[] peerReviewContracts;
+
+    function createPeerReviewContract(address initJournal, bytes initFileHash) payable {
+        // https://solidity.readthedocs.io/en/develop/control-structures.html#creating-contracts-via-new
+        // Cannot limit the amount of gas
+        
+        PeerReview newPR = (new PeerReview).value(msg.value)(initJournal, initFileHash)
+    }
 }
