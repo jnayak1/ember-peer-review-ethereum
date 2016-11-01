1d38ce9d8688de6a8171332c213a7284e5f04491
Jesse Nayak <jdn4ae@virginia.edu>
2016-11-01 18:19:46 -0400

    Create web3js service

--- app/routes/list-contracts.js
+++ app/routes/list-contracts.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
+import Web3 from 'npm:web3';
+
+var contractParams = {
+    journalAddress: '',
+    amountToSend: 0,
+    fileHash: ''
+}
+
+var web3 = new Web3();
+// set rpc
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 export default Ember.Route.extend({
 });
