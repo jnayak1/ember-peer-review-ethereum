e813aed5bb362dc7c4f5c298da570ae7e2f3ae50
Jesse Nayak <jdn4ae@virginia.edu>
2016-11-02 11:24:42 -0400

    Inject web3js service into routes

--- app/routes/journals.js
+++ app/routes/journals.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    web3js: Ember.inject.service('web3js'),
     model(){
     }
 });
--- app/routes/list-contracts.js
+++ app/routes/list-contracts.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    web3js: Ember.inject.service('web3js')
 });
