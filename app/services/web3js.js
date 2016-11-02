71a613b86479f42307620c6686817f5a28b8c70d
Jesse Nayak <jdn4ae@virginia.edu>
2016-11-02 11:06:03 -0400

    Add web3js service

--- app/services/web3js.js
+++ app/services/web3js.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import Web3 from 'npm:web3'; 
 
 export default Ember.Service.extend({
+    web3: null,
+
+    init() {
+        this._super(...arguments);
+        var web3 = new Web3();
+        web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+        this.set('web3', web3);
+    }
 });
